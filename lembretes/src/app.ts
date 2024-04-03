33071357bfdffd918b73dda76cfa55a4bbc60367
Vitoria Helen <vitoriahellen3004@gmail.com>
2024-04-03 17:23:02 -0300

    fear(ap01-mss): encerra o codigo 4.3.10
    - implementa endpoint de adicao de observacoes de lembretes
    - implementa endpoint de  obtencao de observacoes de lembretes

--- lembretes/src/app.ts
+++ lembretes/src/app.ts
@@ -1,2 +1,55 @@
-let hello: string = "Hello, Typescript";
-console.log(hello)
+import express from 'express'; 
+const app = express()
+app.use(express.json())
+interface Lembrete{
+    id: string;
+    texto: string;
+}
+const lembretes: Record <string, Lembrete> = {} 
+let id: string = '1'
+/*
+    {
+        "1":{
+            "id": "1"
+            "texto": "Fazer café"
+        },
+        "2":{
+            "id": "2"
+            "texto": "Nadar"
+        }
+    }
+*/
+
+//get /lembretes obter a coleção de lembretes
+app.get('/lembretes', (req, res) => {
+    res.json(lembretes)
+})
+
+//POST /lembretes cadastrar novo lembrete 
+app.post('/lembretes', (req, res) =>{
+    //extrair texto do corpo da requisição {"texto": "Fazer café"}
+    const { texto } = req.body 
+    //construir um novo lembrete 
+    const lembrete = { id, texto }
+    //armazenar novo lembrete
+    lembretes[id] = lembrete
+    //incremento o id 
+    id = (+id+1).toString()
+    //responder ao cliente
+    res.json(lembrete)
+})
+
+//get /lembretes/{id}
+app.get('/lembretes/:id', (req,res) => { 
+    const { id } = req.params; 
+    const lembrete = lembretes[id]; 
+    if (lembrete) { 
+        res.json(lembrete);
+    } 
+    else { 
+        res.status(404).send('Lembrete nao encontrado'); 
+    } })
+
+
+const port = 4000
+app.listen(port, ()=> console.log(`Lembretes. Porta ${port}.`))
--- observacoes/src/app.ts
+++ observacoes/src/app.ts
@@ -1,2 +1,54 @@
-let hello: string = "Hello, Typescript";
-console.log(hello)
+import express from 'express' 
+//import uuid from 'uuid'
+import {v4 as uuidv4} from 'uuid'
+const app = express()
+app.use(express.json())
+
+
+/* 
+a estrutura da base de observacoes 
+{
+    "1": [
+        {
+            "id": "100", 
+            "texto": "Comprar açucar"
+        },
+        {
+            "id": "101", 
+            "texto": "Ver um filme"
+        }
+    ]
+}
+*/
+
+interface Observacao{
+    id: string;
+    texto: string; 
+}
+const observacoes: Record <string, Observacao[]> = {}
+
+
+//POST /lembretes/123456/observacoes
+app.post('/lembretes/:id/observacoes', (req, res) => {
+   //1. gerar id de observacoes (descobrir como usar a versao 4 do uuid typescript)
+   let idObs = uuidv4();
+   //2. extrair texto do corpo da requisicao
+    const { texto } = req.body
+   //3. pegar a colecao de observacoes do lembrede cujo o id se encontra na url, caso exista
+   //Caso contrário, pego uma colecao nova, vazia 
+    const observacoesDoLembrete: Observacao[] = observacoes[req.params.id] || []
+   //4. Na colecao pega no passo anterior adiciona um novo objeto caracterizado por id e texto
+    observacoesDoLembrete.push({id: idObs, texto})
+   //5. Responder para o cliente com status 201 e entregando a ele a colecao atualizada
+   res.status(201).json(observacoesDoLembrete)
+
+})
+
+app.get('lembretes/:id/observacoes', (req, res) => {
+    res.json(observacoes[req.params.id] || [])
+})
+
+const port = 5000
+app.listen(port, () => {
+    console.log(`Observacoes. ${port}`)
+})
